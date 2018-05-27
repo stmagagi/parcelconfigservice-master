0588684f4e9e4e980d8ac933f533d29132c89847
Paul Buskovskij <paul.buskovskij@gmail.com>
2018-05-28 01:26:07 +0200

    trustURl

--- js/parcel-size.component.js
+++ js/parcel-size.component.js
@@ -1,6 +1,8 @@
 angular.module('parcelConfig').component('parcelSize', {
     transclude: true,
-    templateUrl:  "/ui/html/parcelSize.html",
+    templateUrl:  ["$sce", function ($sce) {
+        return $sce.trustAsResourceUrl("http://192.168.50.11/ui/html/parcelSize.html")
+    }],
     controller: function ParcelSizeController($scope, $http) {
         this.size = {
             length: 3,
@@ -9,7 +11,7 @@ angular.module('parcelConfig').component('parcelSize', {
         };
         this.getGirthSize = function () {
             $http({
-                url: "http://192.168.50.11:8082/api/getGirthSize",
+                url: "http://192.168.50.10:8082/api/getGirthSize",
                 method: "GET",
                 params: {size: this.size}
             }).then(function (res) {
